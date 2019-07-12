8f790f7a977e75ffc4b9b402c82ffe5717411b7e
Bruce He <bruce@shbewell.com>
2019-07-12 17:29:24 +0800

    pip 打包setup.py文件

---
+++ setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+cmdclass = {}
+cmdclass['test'] = PyTest
+
+with open('README.md', 'rb') as f:
+    long_description = f.read().decode('utf-8')
+
+with open('requirements.txt') as f:
+    requirements = [l for l in f.read().splitlines() if l]
+
+setup(
+    name='ebaipy',
+    version='0.1',
+    author='Bruce He',
+    author_email='bruce@shbewell.com',
+    url='https://github.com/brucehe3/ebaipy',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    keywords='Ebai, Eleme, SDK',
+    description='Ebai SDK for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    include_package_data=True,
+    tests_require=[
+        'pytest',
+    ],
+    cmdclass=cmdclass,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
+)
